9f235103081fd2f55819243bca9d278d154518d5
Süleyman Ali Akpınar <suleyman@linkadoo.me>
2021-05-19 15:53:26 +0300

    user service added

--- Backend-Service/api/controllers/user.js
+++ Backend-Service/api/controllers/user.js
@@ -3,14 +3,99 @@
 //const jwt = require ('jsonwebtoken');
 
 
-exports.userCreate = async (req,res,next) => {
-    res.status(200).json({message : "aslkjdalsd"})
-}
-
-exports.userLogin = async (req,res,next) => {
-    res.status(200).json({message : "aslkasdjdalsd"})
-}
-
 exports.userLogout = async (req,res,next) => {
     res.status(200).json({message : "aslkasdjdalsd"})
 }
+
+const User = require('../models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require ('jsonwebtoken');
+
+
+exports.userCreate = (req,res,next) => {
+    User.find({email : req.body.email}).exec()
+    .then(user => {
+        if (user.length >= 1){
+            return res.status(409).json({
+                message : 'Mail exist'
+            })
+        }else {
+            bcrypt.hash(req.body.password, 10, (err,hash)=>{
+                if (err){
+                    res.status(500).json({
+                        error: err
+                    });
+                }else{
+                    const user = new User({
+                        _id : new mongoose.Types.ObjectId(),
+                        email : req.body.email,
+                        password : hash
+                    });
+                    user.save().then(result=>{
+                        console.log("User Created Succesfully")
+                            res.status(201).json({
+                            message : 'User created',
+                            user : result
+                        });
+                    }).catch(err=>{
+                        console.log(err);
+                        res.status(500).json({
+                            error : err
+                        })
+                    })
+                }
+            })
+        }
+    })
+    .catch()
+  
+  
+}
+
+
+
+exports.userLogin = (req, res, next) => {
+    User.find({ email: req.body.email })
+      .exec()
+      .then(user => {
+        if (user.length < 1) {
+          return res.status(401).json({
+            message: "Auth failed"
+          });
+        } 
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+          if (err) {
+            return res.status(401).json({
+              message: "Auth failed"
+            });
+          }
+          if (result) {
+            console.log('User Login Succesfully')
+            const token = jwt.sign(
+              {
+                email: user[0].email,
+                userId: user[0]._id
+              },
+              process.env.JWT_KEY,
+              {
+                  expiresIn: "1h"
+              }
+            );
+            return res.status(200).json({
+              message: "Auth successful",
+              token: token
+            });
+          }
+          res.status(401).json({
+            message: "Auth failed"
+          });
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+  }
--- Backend-Service/app.js
+++ Backend-Service/app.js
@@ -2,17 +2,17 @@ const express = require ('express');
 const app = express();
 const morgan = require ('morgan');
 const bodyParser = require ('body-parser');
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 //require('dotenv').config({path: __dirname + '/.env'});
 
 
 const UserRoutes = require('./api/routes/user');
 
-/* mongoose.connect('mongodb://localhost:27018',{
+mongoose.connect('mongodb://bauproje:bahcesehir3434i@cluster0-shard-00-00.ed8qj.mongodb.net:27017,cluster0-shard-00-01.ed8qj.mongodb.net:27017,cluster0-shard-00-02.ed8qj.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-a6f8vz-shard-0&authSource=admin&retryWrites=true&w=majority',{
     useNewUrlParser: true ,useUnifiedTopology: true ,useCreateIndex :true
 }).then(() => console.log('Auth-Mongo DB connection is Done...'))
-.catch(err =>console.log(err)); */
+.catch(err =>console.log(err)); 
 
 
 
